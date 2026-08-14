19dff33bb5274481be6282cd1b2e90090b418af5
Tio Mahesa <tiomahesa.work@gmail.com>
2026-08-14 09:58:45 +0800

    Refactor code structure for improved readability and maintainability

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -3,9 +3,13 @@
 ])
 
 @if($sidebar)
-    <flux:sidebar.brand :name="config('app.name', 'Laravel')" {{ $attributes }}>
-        <x-slot name="logo" class="flex aspect-square size-8 items-center justify-center rounded-lg bg-teal-500 text-white shadow-sm">
-            <x-app-logo-icon class="size-5 fill-current text-white" />
+    <flux:sidebar.brand {{ $attributes }} class="flex-1! justify-center! h-auto!">
+        <x-slot name="logo" class="h-20 w-auto in-data-flux-sidebar-collapsed-desktop:hidden!">
+            <img
+                src="{{ asset('img/logowhiteapcn.png') }}"
+                alt="{{ config('app.name', 'Laravel') }}"
+                class="h-20 w-auto"
+            />
         </x-slot>
     </flux:sidebar.brand>
 @else
